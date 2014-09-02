7fe755d01c03fad45b92e2a028202b31d9ff2885
jekil <alessandro@tanasi.it>
2014-09-02 23:28:35 +0200

    Initial import

---
+++ scripts/ghiro.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Setup mongo.
+apt-get -y install mongodb
+
+# Setup python stuff.
+apt-get -y install python-pip
---
+++ scripts/zerodisk.sh
@@ -0,0 +1,3 @@
+# Used to help disk compression.
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
