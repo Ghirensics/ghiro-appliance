e564aac719888997b980fa27c442c09bf6fc212f
jekil <alessandro@tanasi.it>
2014-09-04 01:20:13 +0200

    More

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -eux
 
+# Set variables.
+export HISTSIZE=0
+locale-gen en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
 apt-get update
 apt-get -y upgrade
 
+# Basic stuff.
+apt-get install -y vim git zip unzip curl wget
--- scripts/ghiro.sh
+++ scripts/ghiro.sh
@@ -3,5 +3,21 @@
 # Setup mongo.
 apt-get -y install mongodb
 
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password ghiromanager'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password ghiromanager'
+
+apt-get install -y mysql-server
+
 # Setup python stuff.
-apt-get -y install python-pip
+apt-get install -y python-pip build-essential python-dev python-gi
+apt-get install -y libgexiv2-2 gir1.2-gexiv2-0.10
+# Pillow
+apt-get install -y libtiff4-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev python-tk
+
+cd /var/www
+git clone https://github.com/Ghirensics/ghiro.git
+cd ghiro
+
+pip install -r requirements.txt
+
+python manage.py syncdb
