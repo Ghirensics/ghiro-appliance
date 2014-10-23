596e947dfd31805a7d9fdb185353e7c3877b3b22
jekil <alessandro@tanasi.it>
2014-10-24 00:31:24 +0200

    Added apache and uinit configuration

--- scripts/ghiro.sh
+++ scripts/ghiro.sh
@@ -29,3 +29,52 @@ python manage.py syncdb --noinput
 
 # Create super user.
 echo "from users.models import Profile; Profile.objects.create_superuser('ghiro', 'yourmail@example.com', 'ghiromanager')" | python manage.py shell
+
+# Install Apache.
+apt-get install -y apache2 libapache2-mod-wsgi
+
+# Remove default virtualhost.
+a2dissite default
+
+# Configure mod_wsgi and default virtual host.
+cat <<EOF > /etc/apache2/sites-available/ghiro
+    <VirtualHost *:80>
+        ServerAdmin webmaster@localhost
+        WSGIProcessGroup ghiro
+        WSGIDaemonProcess ghiro processes=5 threads=10 user=nobody group=nogroup python-path=/var/www/ghiro/ home=/var/www/ghiro/ display-name=local
+        WSGIScriptAlias / /var/www/ghiro/ghiro/wsgi.py
+        Alias /static/ /var/www/ghiro/static/
+        <Location "/static/">
+            Options -Indexes
+        </Location>
+
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+
+        # Possible values include: debug, info, notice, warn, error, crit,
+        # alert, emerg.
+        LogLevel warn
+
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+    </VirtualHost>
+EOF
+
+# Enable ghiro virtualhost.
+a2ensite ghiro
+
+# Restart Apache.
+service apache2 restart
+
+# Adding processor init script.
+cat <<EOF > /etc/init/ghiro.conf
+description     "Ghiro"
+
+start on started mysql
+stop on shutdown
+script
+    chdir /var/www/ghiro/
+    exec /usr/bin/python manage.py process
+end script
+EOF
+
+# Start ghiro service.
+service ghiro start
