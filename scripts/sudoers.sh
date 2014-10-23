130aab2655878382166cecb053f3cce1f84c6c5b
jekil <alessandro@tanasi.it>
2014-10-23 23:52:21 +0200

    Commented

--- scripts/sudoers.sh
+++ scripts/sudoers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -eux
 
+# Add ghiro user to sudo without asking for a password.
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
 sed -i -e 's/%sudo\s*ALL=(ALL:ALL) ALL/%sudo\tALL=(ALL) NOPASSWD:ALL/g' /etc/sudoers
