29b74e0f26323335ab01c68ec79232078ddbc6a0
jekil <alessandro@tanasi.it>
2014-10-24 00:08:16 +0200

    Removed obsolete rm

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -8,7 +8,5 @@ echo "cleaning up dhcp leases"
 rm /var/lib/dhcp/*
 
 echo "cleaning up udev rules"
-rm /etc/udev/rules.d/70-persistent-net.rules
-mkdir /etc/udev/rules.d/70-persistent-net.rules
 rm -rf /dev/.udev/
 rm /lib/udev/rules.d/75-persistent-net-generator.rules
