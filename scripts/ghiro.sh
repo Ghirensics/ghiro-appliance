36c08f50e14b757fe65da774502016d3da239b53
jekil <alessandro@tanasi.it>
2015-08-26 01:46:50 +0200

    Fixed script to automatically detect IP

--- scripts/ghiro.sh
+++ scripts/ghiro.sh
@@ -172,7 +172,7 @@ service ghiro start
 cat <<EOF > /etc/network/if-up.d/ghirobanner
 #!/bin/sh
 
-IP=\`/sbin/ifconfig | grep "inet addr" | grep -v "127.0.0.1" | awk '{ print $2 }' | awk -F: '{ print $2 }'\`
+IP=\`/sbin/ifconfig | grep "inet addr" | grep -v "127.0.0.1" | awk '{ print \$2 }' | awk -F: '{ print \$2 }'\`
 cat <<FOO > /etc/issue
 ###############################
 # Welcome to Ghiro Appliance! #
