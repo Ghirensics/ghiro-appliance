11d058a26c71dcce649e445e63e3b92effa49449
jekil <alessandro@tanasi.it>
2015-08-27 20:43:14 +0200

    After merge adapted for master use

--- scripts/ghiro.sh
+++ scripts/ghiro.sh
@@ -32,7 +32,6 @@ ln -s /usr/bin/wkhtmltopdf.sh /usr/local/bin/wkhtmltopdf
 cd /var/www
 git clone https://github.com/Ghirensics/ghiro.git
 cd ghiro
-git checkout tags/ghiro-0.2.1
 
 # Configure ghiro
 cat <<EOF > ghiro/local_settings.py
