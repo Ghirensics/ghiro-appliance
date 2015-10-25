e67d2e0b1b4e8a65e6bcb1982090bf5cb2737500
jekil <alessandro@tanasi.it>
2015-10-25 17:29:38 +0100

    Improved wiping

--- scripts/zerodisk.sh
+++ scripts/zerodisk.sh
@@ -1,3 +1,19 @@
-# Used to help disk compression.
-dd if=/dev/zero of=/EMPTY bs=4M
+# Whiteout /boot
+echo "Wiping /boot"
+count=`df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}'`;
+count=$((count -= 1))
+dd if=/dev/zero of=/boot/whitespace bs=1024 count=$count;
+rm /boot/whitespace;
+ 
+# Whiteout swap 
+echo "Wiping swap"
+swappart=`cat /proc/swaps | tail -n1 | awk -F ' ' '{print $1}'`
+swapoff $swappart;
+dd if=/dev/zero of=$swappart;
+mkswap $swappart;
+swapon $swappart;
+
+# Wipe the rest, it also = helps disk compression.
+echo "Wiping /"
+dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
