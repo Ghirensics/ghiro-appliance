ce5e1ab6863d9bcfcffa3da1ab55cba680a185df
jekil <alessandro@tanasi.it>
2015-10-25 17:30:18 +0100

    Comment fix

--- scripts/zerodisk.sh
+++ scripts/zerodisk.sh
@@ -1,11 +1,11 @@
-# Whiteout /boot
+# Wipe /boot
 echo "Wiping /boot"
 count=`df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}'`;
 count=$((count -= 1))
 dd if=/dev/zero of=/boot/whitespace bs=1024 count=$count;
 rm /boot/whitespace;
  
-# Whiteout swap 
+# Wipe swap 
 echo "Wiping swap"
 swappart=`cat /proc/swaps | tail -n1 | awk -F ' ' '{print $1}'`
 swapoff $swappart;
