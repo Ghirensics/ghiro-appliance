04a9c4e260999fcc19fe046181140c737a99a6f1
jekil <alessandro@tanasi.it>
2015-10-25 17:22:35 +0100

    Ssh tweak

--- scripts/base.sh
+++ scripts/base.sh
@@ -13,6 +13,9 @@ apt-get -y upgrade
 # Basic stuff.
 apt-get install -y vim git zip unzip curl wget screen htop
 
+# Tweak sshd to prevent DNS resolution (speed up logins)
+echo 'UseDNS no' >> /etc/ssh/sshd_config
+
 # Fix for Ubuntu/Virtualbox shit.
 # Starting from 14.04.3 in virtualbox we got a black screen each reboot.
 # This is used to re-init the video.
